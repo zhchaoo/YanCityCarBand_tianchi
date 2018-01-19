8fad551acc34b65336c79f0b6aceee9e957ac5bc
周超 <zc98770@alibaba-inc.com>
2018-01-19 18:37:06 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,19 @@
+# data file path
+train_file_path = './data/train_20171215.txt'
+testA_file_path = './data/test_A_20171225.txt'
+testB_file_path = './data/test_B_20171225.txt'
+# data processed
+train_fix_file_path = './data/train_fix.csv'
+test_fix_file_path = './data/test_fix.csv'
+
+# label name
+date_label = 'date'
+day_of_week_label = 'day_of_week'
+brand_label = 'brand'
+cnt_label = 'cnt'
+
+date_fix_label = 'date_fix'
+diff7_label = 'diff7'
+last7_label = 'last7'
+week_label = 'week'
+month_label = 'month'
