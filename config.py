54eacb7c0fae81cc390af2a120c7af60cbd3e634
周超 <zc98770@alibaba-inc.com>
2018-03-07 20:57:41 +0800

    增加复赛B榜代码

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ answerA_file_path = './data/answer_A_20180225.txt'
 fusai_train_file_path = './data/fusai_train_20180227.txt'
 fusai_testA_file_path = './data/fusai_test_A_20180227.txt'
 fusai_testB_file_path = './data/fusai_test_B_20180227.txt'
+fusai_answerA_file_path = './data/fusai_answer_a_20180307.txt'
 # data processed
 train_fix_file_path = './data/train_fix.csv'
 test_fix_file_path = './data/test_fix.csv'
@@ -15,6 +16,7 @@ answerA_fix_file_path = './data/answer_A_fix.csv'
 fusai_train_fix_file_path = './data/fusai_train_fix_20180227.csv'
 fusai_testA_fix_file_path = './data/fusai_test_A_fix_20180227.csv'
 fusai_testB_fix_file_path = './data/fusai_test_B_fix_20180227.csv'
+fusai_answerA_fix_file_path = './data/fusai_answer_a_fix.csv'
 
 # label name
 date_label = 'date'
