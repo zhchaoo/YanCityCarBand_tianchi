1bb33f5ae772c2ccc80a6d839af7f4f91e1da8ff
周超 <zc98770@alibaba-inc.com>
2018-03-08 20:16:45 +0800

    引入复赛A组数据答案

--- config.py
+++ config.py
@@ -2,17 +2,21 @@
 train_file_path = './data/train_20171215.txt'
 testA_file_path = './data/test_A_20171225.txt'
 testB_file_path = './data/test_B_20171225.txt'
+answerA_file_path = './data/answer_A_20180225.txt'
 
 fusai_train_file_path = './data/fusai_train_20180227.txt'
 fusai_testA_file_path = './data/fusai_test_A_20180227.txt'
 fusai_testB_file_path = './data/fusai_test_B_20180227.txt'
+fusai_answerA_file_path = './data/fusai_answer_a_20180307.txt'
 # data processed
 train_fix_file_path = './data/train_fix.csv'
 test_fix_file_path = './data/test_fix.csv'
+answerA_fix_file_path = './data/answer_A_fix.csv'
 
 fusai_train_fix_file_path = './data/fusai_train_fix_20180227.csv'
 fusai_testA_fix_file_path = './data/fusai_test_A_fix_20180227.csv'
 fusai_testB_fix_file_path = './data/fusai_test_B_fix_20180227.csv'
+fusai_answerA_fix_file_path = './data/fusai_answer_a_fix.csv'
 
 # label name
 date_label = 'date'
